03683062f1abbe48ac73ba2b7d6501854f410bff
Aaryaan Gandhi <a21gandhi@gmail.com>
2023-12-12 15:09:46 +0530

    changes in ts file

--- src/app/admin-panel/admin/admin.component.ts
+++ src/app/admin-panel/admin/admin.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss','./admin2.component.scss','./admin3.component.scss']
 })
 export class AdminComponent {
 
